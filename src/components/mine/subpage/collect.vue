859427b963502db83dc8af404b5f474d46edf85c
罗莎 <88888888@qq.com>
2017-05-31 19:21:41 +0800

    wodeshoucang

--- src/components/mine/subpage/collect.vue
+++ src/components/mine/subpage/collect.vue
@@ -1,14 +1,534 @@
 <template>
   <div>
     <x-header v-on:click="$router.back()">我的收藏</x-header>
+    <div class="card-demo-flex card-demo-content">
+      <div >
+        <group>
+          <cell title="商家" is-link
+                :border-intent="false"
+                :arrow-direction="showContent001 ? 'up' : 'down'"
+                @click.native="show1">
+          </cell>
+        </group>
+      </div>
+      <div >
+        <group>
+          <cell
+            title="菜品"
+            is-link
+            :border-intent="false"
+            :arrow-direction="showContent002 ? 'up' : 'down'"
+            @click.native="show2"></cell>
+        </group>
+      </div>
+      <div>
+        <group>
+          <cell
+            title="文章"
+            is-link
+            :border-intent="false"
+            :arrow-direction="showContent003 ? 'up' : 'down'"
+            @click.native="show3"></cell>
+        </group>
+      </div>
+    </div>
+
+    <template v-if="showContent001" style="position: fixed">
+      <!--<scroller enable-horizontal-swiping=""  :loading="load" >-->
+        <swiper :list="list0" :min-moving-distance="20" auto="" style="height:200px;margin: 15px">
+        </swiper>
+      <!--</scroller>-->
+      <p class="B-text">【美味大数据】今天你挑哪家？</p>
+      <hr>
+      <div style=" position: relative;margin: 10px;overflow: hidden; width: 45%;float: left;height: 150px;" v-for="item in list1">
+        <div class="m-img1" :style="{backgroundImage: 'url(' + item.img + ')'}"></div>
+          <div slot="content" class="m-title" >
+            <p>{{item.title}}</p>
+          </div>
+      </div>
+     </template>
+    <template  v-if="showContent002" style="position: fixed">
+        <!--<swiper :list="list01" :min-moving-distance="100" auto="" height="230px" style="height:230px;margin: 15px 15px 0 15px ">-->
+        <!--</swiper>-->
+      <swiper loop auto :list="list01"
+              :index="list01"></swiper>
+　　
+
+      <div style=" position: relative;margin:  21px -8px 13px 13px;overflow: hidden; width: 28%;float: left;height: 140px;" v-for="item in list2">
+        <p class="F-date">{{item.date}}</p>
+        <div class="m-img" :style="{backgroundImage: 'url(' + item.img + ')'}"></div>
+        <div slot="content" class="m-title" ></div>
+      </div>
+
+</template>
+    <template v-if="showContent003">
+      <scroller enable-horizontal-swiping=""  :loading="load" >
+        <swiper :list="list02" :min-moving-distance="20" auto="" style="height:200px;margin: 15px 15px 0 15px ">
+        </swiper>
+      </scroller>
+      <!--11111111111111111-->
+      <div class="article">
+      <div class="takeSorting" @click="GoBusiness (item)" v-for="item in list3" >
+        <div class="sortingl" :style="{backgroundImage: 'url(' + item.img + ')'}"></div>
+        <div class="sortingr">
+          <span class="T-title">{{item.title}}</span>
+          <span class="T-time">{{item.time}}</span>
+          <br>
+          <span class="T-type">{{item.type}}</span>
+          <br>
+          <!--222222222222222222-->
+          <div @click="GoBusiness (item)" v-for="item in list4">
+            <div class="address" >
+              <a href="">
+              <div class="address-img" :style="{backgroundImage: 'url(' + item.img + ')'}"></div>
+              <p class="add-name">{{item.name}}</p>
+              <p class="add-add1">{{item.address1}}</p>
+              </a>
+            </div>
+            <p class="add-add2">{{item.address2}}</p>
+           <span class="add-add3">浏览</span> <span class="add-add4">{{item.look}}</span>
+            <!--3333333333333333333-->
+            <div  class="comment" @click="GoBusiness (item)" v-for="item in list5">
+            <div class="comment-img" :style="{backgroundImage: 'url(' + item.img + ')'}"></div>
+            <a href="" class="comment-text">{{item.comment}}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </template>
+
   </div>
+
 </template>
 
 <script>
-  import { XHeader } from 'vux'
+  import { Swiper, Scroller, FlexboxItem, Flexbox, Divider, CellBox, Card, Masker, XHeader, TransferDom, Group, Cell, Panel, Rater, Badge } from 'vux'
+  import img2 from '../../../assets/images/B1-2.png'
+  import img3 from '../../../assets/images/B1-3.png'
+  import img4 from '../../../assets/images/B1-5.png'
+  import img5 from '../../../assets/images/B1-4.png'
+  import img1 from '../../../assets/images/B1-1.png'
+  import img6 from '../../../assets/images/F2-1.png'
+  import img7 from '../../../assets/images/F2-2.png'
+  import img8 from '../../../assets/images/F2-3.png'
+  import img9 from '../../../assets/images/F2-4.png'
+  import img10 from '../../../assets/images/F2-5.png'
+  import img11 from '../../../assets/images/F2-6.png'
+  import img12 from '../../../assets/images/F2-7.png'
+  import img13 from '../../../assets/images/T3-1.png'
+  import img14 from '../../../assets/images/T3-2.png'
+  import img15 from '../../../assets/images/T3-3.png'
+  import img16 from '../../../assets/images/comment.jpg'
   export default {
+    directives: {
+      TransferDom
+    },
     components: {
-      XHeader
+      Masker,
+      XHeader,
+      Group,
+      Cell,
+      Card,
+      CellBox,
+      Panel,
+      Rater,
+      Badge,
+      Divider,
+      Flexbox,
+      FlexboxItem,
+      Scroller,
+      Swiper,
+      first: {
+        template: '<p>电话1{{tel}}</p>'
+      },
+      second: {
+        template: '<p>电话2}</p>'
+      },
+      third: {
+        template: '<p>电话3}</p>'
+      }
+    },
+    data () {
+      return {
+        showContent001: true,
+        showContent002: false,
+        showContent003: false,
+        tel: 1232132,
+        value2: 'vux',
+        list0: [{
+          url: '../../../assets/images/B1-1.jpg',
+          img: img1
+        }, {
+          url: '../../../assets/images/B1-1.png',
+          img: img1
+        }],
+        list01: [{
+          url: '../../../assets/images/F2-1.jpg',
+          img: img6
+        }],
+        list02: [{
+          url: '../../../assets/images/T3-1.jpg',
+          img: img13
+        }],
+        list1: [{
+          title: '巴西烤肉',
+          url: '../../../assets/images/B1-3.png',
+          img: img3
+        }, {
+          title: '窄巷子砂锅',
+          url: '../../../assets/images/B1-4.png',
+          img: img2
+        }, {
+          title: '柳香园',
+          url: '../../../assets/images/B1-5.png',
+          img: img4
+        }, {
+          title: '外婆的味道',
+          url: '../../../assets/images/B1-2.png',
+          img: img5
+        }, {
+          title: '巴西烤肉',
+          url: '../../../assets/images/B1-3.png',
+          img: img3
+        }, {
+          title: '窄巷子砂锅',
+          url: '../../../assets/images/B1-4.png',
+          img: img2
+        }, {
+          title: '柳香园',
+          url: '../../../assets/images/B1-5.png',
+          img: img4
+        }, {
+          title: '外婆的味道',
+          url: '../../../assets/images/B1-2.png',
+          img: img5
+        }
+        ],
+        list2: [{
+          date: '2017年4月17日',
+          url: '../../../assets/images/B1-3.png',
+          img: img12
+        }, {
+          date: '.',
+          url: '../../../assets/images/B1-4.png',
+          img: img7
+        }, {
+          date: '.',
+          url: '../../../assets/images/B1-5.png',
+          img: img8
+        }, {
+          date: '2017年4月17日',
+          url: '../../../assets/images/B1-2.png',
+          img: img9
+        }, {
+          date: '.',
+          url: '../../../assets/images/B1-3.png',
+          img: img10
+        }, {
+          date: '.',
+          url: '../../../assets/images/B1-5.png',
+          img: img11
+        }
+        ],
+        list3: [{
+          img: img15,
+          title: '小李吃货',
+          time: '2017年4月17日',
+          type: '无意见和同学来到了这家店，菜的味道真心不错，分量足足的，' +
+          '而且餐厅很有情调小清新风格工作人员态度热情，这里交通还方便，一定会再来！'
+        }],
+        list4: [{
+          img: img14,
+          url: '../../../assets/images/T3-2.png',
+          name: '湘村馆（七彩俊园）',
+          address1: '环城东路',
+          address2: '昆明理工大学  新迎校区',
+          look: '1314'
+        }],
+        list5: [{
+          comment: '22',
+          img: img16,
+          url: '../../../assets/images/comment.jpg'
+        }]
+      }
+    },
+    mounted () {
+      // 进入页面的钩子函数
+    },
+    methods: {
+      business_info (item) {
+        alert(item.title)
+      },
+      show1 () {
+        this.showContent001 = !this.showContent001
+        this.showContent003 = false
+        this.showContent002 = false
+      },
+      show2 () {
+        this.showContent002 = !this.showContent002
+        this.showContent003 = false
+        this.showContent001 = false
+      },
+      show3 () {
+        this.showContent003 = !this.showContent003
+        this.showContent002 = false
+        this.showContent001 = false
+      },
+      GoBusiness (item) {
+        this.$router.push({name: 'business'})
+        alert(item.title)
+      },
+      load (uuid) {
+        const _this = this
+        setTimeout(function () {
+          _this.$broadcast('pulldown:reset', uuid)
+        }, 2000)
+      },
+      onSubmit (val) {
+        window.alert('on submit' + val)
+      },
+      onCancel () {
+        console.log('on cancel')
+      },
+      onFocus () {
+        console.log('on focus')
+      }
     }
   }
 </script>
+
+<style scoped="" lang="less">
+  .article{
+    position: relative;
+  }
+  .comment{
+    margin-top: -34px;
+    margin-left: 70px;
+  }
+  .comment-img{
+    width: 29Px;
+    height: 21px;
+    position: absolute;
+    left: 230px;
+    top: 260px;
+    background-size: cover;
+    background-color: #9b9b9b;
+    display: inline-block;
+  }
+  .comment-text{
+    border: 1px solid #878787;
+    border-radius: 14px;
+    padding: 2px 7px 2px 48px;
+    position: absolute;
+    left: 220px;
+    top:250px;
+    font-size: 20px;
+  }
+  .add-add4{
+    color: #878787;
+    margin-top: 5px;
+    position: absolute;
+    top:260px;
+    left: 30px;
+  }
+  .add-add3{
+    color: #878787;
+    margin-top: 5px;
+    position: absolute;
+    top:260px;
+    left: 0;
+  }
+  .add-add2{
+    color: #878787;
+    margin-top: 5px;
+    position: absolute;
+    top:240px;
+    left: 0;
+  }
+  .add-add1{
+    margin-left: 73px;
+    font-size:16px;
+    color: #878787;
+    margin-bottom: 5px;
+  }
+  .address{
+    position: absolute;
+    top:137px;
+    left: 0;
+    margin-top: 15px;
+    border: 1px solid #E8E8E8;
+    background: #E8E8E8;
+  }
+  .add-name{
+    margin-left: 75px;
+    margin-top: 6px;
+    font-size: 22px;
+    margin-bottom: 10px;
+  }
+  .address-img{
+    width: 60Px;
+    height: 60px;
+    position: absolute;
+    left: 11px;
+    top: 10px;
+    background-size: cover;
+    background-color: #9b9b9b;
+    display: inline-block;
+  }
+  .F-date{
+   margin-top: 10px;
+  }
+  .m-img {
+    display: inline-block!important;
+    min-width: 90%;
+    max-width: 90%;
+    height: 110px;
+    overflow: hidden;
+    background-size: cover;
+    cursor: pointer;
+    border-radius: 2px;
+    margin-left: 10px;
+
+  }
+  .m-img1 {
+    display: inline-block!important;
+    min-width: 90%;
+    max-width: 90%;
+    height: 150px;
+    overflow: hidden;
+    background-size: cover;
+    cursor: pointer;
+    border-radius: 2px;
+    margin-left: 10px;
+
+  }
+  .m-title {
+    color: #fff;
+    text-align: center;
+    text-shadow: 0 0 2px rgba(0, 0, 0, 0.5);
+    font-weight: 500;
+    font-size: 16px;
+    position: absolute;
+    left: 5%;
+    top: 91%;
+    width: 168px !important;
+    text-align: center;
+    background: rgba(37,37,38,.5);
+  }
+
+  .m-time {
+    font-size: 12px;
+    padding-top: 4px;
+    border-top: 1px solid #f0f0f0;
+    display: inline-block;
+    margin-top: 5px;
+  }
+  a{
+    text-align: center;
+  }
+
+  /*********************/
+  .B-intru{
+    width: 88%;
+    margin-top: 34% ;
+    margin-left: 12px;
+    position: relative;
+
+  }
+
+  .B-text1{
+    position: absolute;
+    top: 67%;
+    left:16%;
+    color: #F9F9FA;
+    font-size: 20px;
+  }
+  i{
+    position: absolute;
+    /*border: 1px solid #0bb20c;*/
+    background: rgba(154,153,153,.5);
+    width: 43%;
+    height: 4%;
+    top: 67%;
+    left: 3%;
+  }
+  .B-text{
+    font-size: 20px;
+    color:#868781 ;
+    margin-bottom: 10px;
+  }
+  .B-img1{
+    width: 95%;
+    margin: 10px;
+  }
+  .card-demo-flex {
+    display: flex;
+  }
+  .card-demo-content01  {
+    padding: 10px 0;
+  }
+  .card-padding {
+    padding: 15px;
+  }
+  .card-demo-flex > div {
+    flex: 1;
+    text-align: center;
+    font-size: 12px;
+  }
+  .takeSorting{
+    width: 100%;
+    height:150px;
+    position: absolute;
+    top:0;
+    margin-bottom: 10px;
+    /*border-bottom: 1px solid #dddbdb;*/
+  }
+  .sortingl{
+    border-radius: 50%;
+    width: 60Px;
+    height: 60px;
+    position: absolute;
+    top: 10px;
+    left: 25px;
+    background-size: cover;
+    background-color: #9b9b9b;
+    display: inline-block;
+  }
+  .sortingr{
+    width: 78%;
+    margin-right: 0px;
+    margin-top: 10px;
+    height: 150px;
+    padding:20px 5px;
+    position: absolute;
+    top:0;
+    right:0;
+    display: inline-block;
+  }
+  .T-title{
+    font-size: 20px;
+    color: #315D83;
+  }
+  .T-time{
+    font-size: 15px;
+    color: #8D8D8D;
+    margin: 63px;
+
+  }
+  .T-type{
+    margin: -2px;
+    font-size: 15px;
+  }
+  .sortingl p{
+    display: inline-block;
+  }
+  .sortingimg {
+    width: 100%;
+    height: 100%;
+  }
+  .sortingr .statuss {
+    color: red;
+    margin-right: 2px;
+  }
+</style>
--- src/components/mine/subpage/site.vue
+++ src/components/mine/subpage/site.vue
@@ -1,62 +1,152 @@
 <template>
   <div>
-  <x-header v-on:click="$router.back()">设置</x-header>
+    <x-header v-on:click="$router.back()">设置</x-header>
     <group>
-      <cell title='意见反馈'  is-link></cell>
-      <cell title='用户帮助' is-link></cell>
+      <cell title="意见反馈" @click.native="showPlugin3" is-link></cell>
+      <cell title="用户帮助" @click.native="showPlugin5" is-link></cell>
+      <div v-transfer-dom>
+        <alert v-model="show7" title="蕨菜客服" @on-hide="onHide" content="您需要的帮助？"></alert>
+      </div>
     </group>
     <group>
       <cell title="软件关于" @click.native="showPlugin" is-link></cell>
       <div v-transfer-dom>
         <alert v-model="show2" title="关于" @on-hide="onHide" content="Your Message is sent successfully"></alert>
       </div>
-      <cell title='清理缓存'  is-link></cell>
-      <cell title='软件分享' is-link></cell>
+      <x-switch title='清除缓存' v-model="show3"></x-switch>
       <cell title="联系我们" @click.native="showPlugin1" is-link></cell>
       <div v-transfer-dom>
         <alert v-model="show1" title="蕨菜客服" @on-hide="onHide" content="400-101-201"></alert>
       </div>
-      <cell title='去评价' is-link></cell>
+      <cell title="去评价" @click.native="showPlugin4" is-link></cell>
     </group>
 
+    <actionsheet v-model="show3" :menus="menus3" @on-click-menu="click" @on-click-menu-delete="onDelete" show-cancel></actionsheet>
+    <toast v-model="delSuccess">{{'清除成功'}}</toast>
 
+    <div v-transfer-dom>
+      <x-dialog v-model="show5" hide-on-blur>
+        <div class="input1"><span class="input2">请输入您的建议</span>
+            <x-textarea :max="20" placeholder="请输入..."
+                        @on-focus="onEvent('focus')"
+                        @on-blur="onEvent('blur')">
+            </x-textarea>
+          <!--<x-button type="primary" @click="console.info('................')">确认提交</x-button>-->
+          <div @click="show5=false">
+            <span class="get">确认提交 </span>
+          </div>
+        </div>
+      </x-dialog>
+    </div>
+    <div v-transfer-dom>
+      <x-dialog v-model="show6" hide-on-blur>
+        <div class="input1"><span class="input2">请输入您的评价</span>
+          <x-textarea :max="20" placeholder="请输入..."
+                      @on-focus="onEvent('focus')"
+                      @on-blur="onEvent('blur')">
+          </x-textarea>
+          <!--<x-button type="primary" @click="console.info('................')">确认提交</x-button>-->
+          <div @click="show6=false">
+            <span class="get">确认提交 </span>
+          </div>
+        </div>
+      </x-dialog>
+    </div>
   </div>
 
 </template>
 <script>
 
-import { XHeader, Alert, Group, Cell, TransferDomDirective as TransferDom } from 'vux'
+  import { XButton, XTextarea, XDialog, XHeader, Actionsheet, Toast, XInput, Alert, Group, XSwitch, Cell, TransferDomDirective as TransferDom } from 'vux'
 
-export default {
-  directives: {
-    TransferDom
-  },
-  components: {
-    XHeader,
-    Alert,
-    Group,
-    Cell
-  },
-  data () {
-    return {
-      show: false,
-      show1: false,
-      show2: false
-    }
-  },
-  methods: {
-    onHide () {
-      console.log('on hide')
+  export default {
+    directives: {
+      TransferDom
     },
-    onShow () {
-      console.log('on show')
+    components: {
+      XHeader,
+      Alert,
+      Group,
+      Cell,
+      XSwitch,
+      Actionsheet,
+      Toast,
+      XInput,
+      XDialog,
+      XTextarea,
+      XButton
     },
-    showPlugin () {
-      this.$set(this, 'show2', true)
+    data () {
+      return {
+        show: false,
+        show1: false,
+        show2: false,
+        show3: false,
+        show5: false,
+        show6: false,
+        show7: false,
+        delSuccess: false,
+        menus3: {
+          'title.noop': '清除缓存吗?<br/><span style="color:#666;font-size:12px;">Once deleted, you will never find it.</span>',
+          delete: '<span style="color:red">清除</span>'
+        }
+      }
     },
-    showPlugin1 () {
-      this.$set(this, 'show1', true)
+    methods: {
+      onHide () {
+        console.log('on hide')
+      },
+      onShow () {
+        console.log('on show')
+      },
+      showPlugin () {
+        this.$set(this, 'show2', true)
+      },
+      showPlugin1 () {
+        this.$set(this, 'show1', true)
+      },
+      showPlugin3 () {
+        this.show5 = true
+      },
+      showPlugin4 () {
+        this.show6 = true
+      },
+      showPlugin5 () {
+        this.$set(this, 'show7', true)
+      },
+      click (key) {
+        console.log(key)
+      },
+      onDelete () {
+        this.delSuccess = true
+      },
+      onEvent (event) {
+        console.log('on', event)
+      }
     }
   }
-}
 </script>
+<style lang="less" scoped>
+  .get {
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    color: #fff;
+    width: 100%;
+    height:40px;
+    font-size: 20px;
+    background-color: #0bb20c;
+
+  }
+  .input1{
+    border: 1px solid #fff;
+    background: #fff;
+  }
+  .input2{
+    font-size: 20px;
+  }
+  .vux-close {
+    margin-top: 8px;
+    margin-bottom: 8px;
+  }
+</style>
